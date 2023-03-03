0334b3471d624dcbb13f23bc30107ee2b3ab6d3c
agkayster <ejike.chiboka@gmail.com>
2023-03-03 18:48:36 +0100

    updated access control allow origin

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
+	res.setHeader(
+		'Access-Control-Allow-Origin',
+		'https://airtel-channel-partner-app.vercel.app'
+	);
 	res.setHeader(
 		'Access-Control-Allow-Methods',
 		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
