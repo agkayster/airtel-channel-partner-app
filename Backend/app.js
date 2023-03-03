d91609a9f828ef9654912dac8eefed567920d03b
agkayster <ejike.chiboka@gmail.com>
2023-03-03 19:18:03 +0100

    updated headers in backend

--- Backend/app.js
+++ Backend/app.js
@@ -32,7 +32,7 @@ app.use(function (req, res, next) {
 	);
 	res.setHeader(
 		'Access-Control-Allow-Headers',
-		'X-Requested-With, content-type'
+		'Origin, X-Requested-With, Content-Type, Accept'
 	);
 	res.setHeader('Access-Control-Allow-Credentials', 'true');
 	next();
