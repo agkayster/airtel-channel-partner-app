c0500c5d7db2d357e7973e02083b950f4be2edd9
agkayster <ejike.chiboka@gmail.com>
2023-03-06 10:02:11 +0100

    implemented validatior match for email in backend models

--- Backend/app.js
+++ Backend/app.js
@@ -10,44 +10,20 @@ require('dotenv').config();
 const notFound = require('./middleware/not-found');
 const errorHandler = require('./middleware/errorHandler');
 const port = require('./config/environment');
-// const corsOptions = {
-// 	origin: 'http://localhost:3000',
-// 	credentials: true,
-// 	optionsSuccessStatus: 200,
-// };
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // middleware
 app.use(express.json());
 app.use(cors());
-// app.use(function (req, res, next) {
-// 	res.setHeader(
-// 		'Access-Control-Allow-Origin',
-// 		'https://airtel-channel-partner-app.vercel.app'
-// 	);
-// 	res.setHeader(
-// 		'Access-Control-Allow-Headers',
-// 		'Origin, X-Requested-With, Content-Type, Accept'
-// 	);
-// 	res.setHeader('Access-Control-Allow-Credentials', true);
-// 	if (req.method === 'OPTIONS') {
-// 		res.setHeader(
-// 			'Access-Control-Allow-Methods',
-// 			'GET, POST, PUT, PATCH, DELETE'
-// 		);
-// 		return res.json({});
-// 	}
-// 	next();
-// });
-// app.use(cors(corsOptions));
-// default route must always be above app.use(notFound)
+
 app.get('/', (req, res) => {
 	res.send('Airtel Registration Portal');
 });
 
 //Routes
 app.use('/api/v1/registrations', registrationsRouter);
+
 // non-existent routes
 app.use(notFound);
 // catching errors after asyncwrapper
--- Backend/models/Registration.js
+++ Backend/models/Registration.js
@@ -47,6 +47,11 @@ const RegistrationSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		required: [true, 'please fill in the email'],
+		match: [
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+			'Please provide a valid email',
+		],
+		unique: true,
 	},
 	website: {
 		type: String,
